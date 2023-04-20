c84337081bd62cf251dc83de8fec185fab8bf2ef
yogi-2024 <yogeshthorat405@gmail.com>
2023-04-20 10:10:47 +0530

    yogesh done by first commit

---
+++ src/main/java/com/Add/calculator/Sum.java
@@ -0,0 +1,13 @@
+package com.Add.calculator;
+
+public class Sum {
+	
+	
+	public int sum(int a, int b)
+	{
+		int c= a+b;
+		System.out.println(" adition service: of a And b " + c);
+		return c;
+		
+	}
+}
---
+++ src/test/java/com/Add/AdditionLogicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Add;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdditionLogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
